adfb83408c85c1ce13db20cfc57261fe8bffa02c
Holger <hdecarne@gmail.com>
2016-11-16 07:02:03 +0100

    Working on certificate import.

--- de.carne.certmgr/src/main/java/de/carne/certmgr/jfx/certimport/CertImportController.java
+++ de.carne.certmgr/src/main/java/de/carne/certmgr/jfx/certimport/CertImportController.java
@@ -52,6 +52,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
@@ -124,6 +125,9 @@ public class CertImportController extends StageController {
 
 	@FXML
 	Label ctlStatusMessage;
+
+	@FXML
+	CheckBox ctlSelectAllOption;
 
 	@FXML
 	TreeTableView<ImportEntryModel> ctlImportEntryView;
