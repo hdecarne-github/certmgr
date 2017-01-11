9f63a03de7bb984c35493a82408e33afa9463c8e
Holger <hdecarne@gmail.com>
2017-01-11 21:34:57 +0100

    Update file comments (for 2017)

--- src/main/java/de/carne/certmgr/certs/security/AbstractAlgorithm.java
+++ src/main/java/de/carne/certmgr/certs/security/AbstractAlgorithm.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Holger de Carne and contributors, All Rights Reserved.
+ * Copyright (c) 2015-2017 Holger de Carne and contributors, All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
