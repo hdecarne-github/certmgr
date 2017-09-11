eb3917e59f432ad20c611e06104162055aa0197f
Holger <hdecarne@gmail.com>
2017-09-11 07:03:05 +0200

    DER reading and writing support completed.

--- src/main/java/de/carne/certmgr/certs/io/DERCertReaderWriter.java
+++ src/main/java/de/carne/certmgr/certs/io/DERCertReaderWriter.java
@@ -22,15 +22,32 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.PrivateKey;
 import java.security.cert.X509CRL;
 import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.pkcs.EncryptedPrivateKeyInfo;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
 import org.bouncycastle.cert.X509CRLHolder;
 import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.jcajce.util.DefaultJcaJceHelper;
+import org.bouncycastle.jcajce.util.JcaJceHelper;
+import org.bouncycastle.operator.InputDecryptorProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.OutputEncryptor;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfoBuilder;
+import org.bouncycastle.pkcs.PKCSException;
+import org.bouncycastle.pkcs.jcajce.JcePKCSPBEInputDecryptorProviderBuilder;
+import org.bouncycastle.pkcs.jcajce.JcePKCSPBEOutputEncryptorBuilder;
 
 import de.carne.certmgr.certs.CertObjectStore;
 import de.carne.certmgr.certs.CertProviderException;
@@ -38,6 +55,7 @@ import de.carne.certmgr.certs.PasswordCallback;
 import de.carne.certmgr.certs.PasswordRequiredException;
 import de.carne.certmgr.certs.spi.CertReader;
 import de.carne.certmgr.certs.spi.CertWriter;
+import de.carne.certmgr.certs.x509.KeyHelper;
 import de.carne.certmgr.certs.x509.PKCS10CertificateRequest;
 import de.carne.check.Nullable;
 import de.carne.util.Exceptions;
@@ -50,6 +68,15 @@ import de.carne.util.logging.Log;
 public class DERCertReaderWriter extends JCAConversion implements CertReader, CertWriter {
 
 	private static final Log LOG = new Log(CertIOI18N.BUNDLE);
+
+	private static final ASN1ObjectIdentifier OUTPUT_ENCRYPTOR_ALGORITHM = PKCSObjectIdentifiers.pbeWithSHAAnd3_KeyTripleDES_CBC;
+
+	private static final JcePKCSPBEOutputEncryptorBuilder OUTPUT_ENCRYPTOR_BUILDER = new JcePKCSPBEOutputEncryptorBuilder(
+			OUTPUT_ENCRYPTOR_ALGORITHM);
+
+	private static final JcePKCSPBEInputDecryptorProviderBuilder INPUT_DECRYPTOR_BUILDER = new JcePKCSPBEInputDecryptorProviderBuilder();
+
+	private static final JcaJceHelper JCA_JCE_HELPER = new DefaultJcaJceHelper();
 
 	/**
 	 * Provider name.
@@ -100,7 +127,7 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 			ASN1Primitive derObject;
 
 			while ((derObject = derStream.readObject()) != null) {
-				X509Certificate crt = decodeCRT(derObject);
+				X509Certificate crt = tryDecodeCRT(derObject);
 
 				if (crt != null) {
 					if (certObjects == null) {
@@ -110,7 +137,17 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 					continue;
 				}
 
-				PKCS10CertificateRequest csr = decodeCSR(derObject);
+				KeyPair key = tryDecodeKey(derObject, in.resource(), password);
+
+				if (key != null) {
+					if (certObjects == null) {
+						certObjects = new CertObjectStore();
+					}
+					certObjects.addKey(key);
+					continue;
+				}
+
+				PKCS10CertificateRequest csr = tryDecodeCSR(derObject);
 
 				if (csr != null) {
 					if (certObjects == null) {
@@ -120,7 +157,7 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 					continue;
 				}
 
-				X509CRL crl = decodeCRL(derObject);
+				X509CRL crl = tryDecodeCRL(derObject);
 
 				if (crl != null) {
 					if (certObjects == null) {
@@ -190,6 +227,7 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 				out.io().write(storeEntry.getCRT().getEncoded());
 				break;
 			case KEY:
+				out.io().write(KeyHelper.encodePrivateKey(storeEntry.getKey().getPrivate()));
 				break;
 			case CSR:
 				out.io().write(storeEntry.getCSR().toPKCS10().getEncoded());
@@ -206,19 +244,13 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 	private static void writeEncryptedCertObject(IOResource<OutputStream> out, CertObjectStore.Entry storeEntry,
 			PasswordCallback newPassword) throws IOException {
 		LOG.debug("Writing encrypted DER object ''{0}'' to resource ''{1}''...", storeEntry, out);
-
-		char[] passwordChars = newPassword.queryPassword(out.resource());
-
-		if (passwordChars == null) {
-			throw new PasswordRequiredException(out.resource());
-		}
-
 		try {
 			switch (storeEntry.type()) {
 			case CRT:
 				out.io().write(storeEntry.getCRT().getEncoded());
 				break;
 			case KEY:
+				out.io().write(encryptKey(storeEntry.getKey(), out.resource(), newPassword));
 				break;
 			case CSR:
 				out.io().write(storeEntry.getCSR().toPKCS10().getEncoded());
@@ -233,7 +265,7 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 	}
 
 	@Nullable
-	private static X509Certificate decodeCRT(ASN1Primitive asn1Object) throws IOException {
+	private static X509Certificate tryDecodeCRT(ASN1Primitive asn1Object) throws IOException {
 		X509CertificateHolder crtObject = null;
 
 		try {
@@ -245,7 +277,65 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 	}
 
 	@Nullable
-	private static PKCS10CertificateRequest decodeCSR(ASN1Primitive asn1Object) throws IOException {
+	private static KeyPair tryDecodeKey(ASN1Primitive asn1Object, String resource, PasswordCallback password)
+			throws IOException {
+		PKCS8EncryptedPrivateKeyInfo encryptedPrivateKeyInfo = null;
+
+		try {
+			encryptedPrivateKeyInfo = new PKCS8EncryptedPrivateKeyInfo(EncryptedPrivateKeyInfo.getInstance(asn1Object));
+		} catch (Exception e) {
+			Exceptions.ignore(e);
+		}
+
+		PrivateKeyInfo privateKeyInfo = null;
+
+		if (encryptedPrivateKeyInfo != null) {
+			Throwable passwordException = null;
+
+			while (privateKeyInfo == null) {
+				char[] passwordChars = password.queryPassword(resource);
+
+				if (passwordChars == null) {
+					throw new PasswordRequiredException(resource, passwordException);
+				}
+
+				InputDecryptorProvider inputDecryptorProvider = INPUT_DECRYPTOR_BUILDER.build(passwordChars);
+
+				try {
+					privateKeyInfo = encryptedPrivateKeyInfo.decryptPrivateKeyInfo(inputDecryptorProvider);
+				} catch (PKCSException e) {
+					passwordException = e;
+				}
+			}
+		}
+
+		try {
+			privateKeyInfo = PrivateKeyInfo.getInstance(asn1Object);
+		} catch (Exception e) {
+			Exceptions.ignore(e);
+		}
+
+		KeyPair key = null;
+
+		if (privateKeyInfo != null) {
+			PrivateKey privateKey;
+
+			try {
+				String algorithmId = privateKeyInfo.getPrivateKeyAlgorithm().getAlgorithm().getId();
+				KeyFactory keyFactory = JCA_JCE_HELPER.createKeyFactory(algorithmId);
+				PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyInfo.getEncoded());
+
+				privateKey = keyFactory.generatePrivate(keySpec);
+			} catch (GeneralSecurityException e) {
+				throw new CertProviderException(e);
+			}
+			key = KeyHelper.rebuildKeyPair(privateKey);
+		}
+		return key;
+	}
+
+	@Nullable
+	private static PKCS10CertificateRequest tryDecodeCSR(ASN1Primitive asn1Object) throws IOException {
 		PKCS10CertificationRequest csrObject = null;
 
 		try {
@@ -257,7 +347,7 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 	}
 
 	@Nullable
-	private static X509CRL decodeCRL(ASN1Primitive asn1Object) throws IOException {
+	private static X509CRL tryDecodeCRL(ASN1Primitive asn1Object) throws IOException {
 		X509CRLHolder crlObject = null;
 
 		try {
@@ -266,6 +356,27 @@ public class DERCertReaderWriter extends JCAConversion implements CertReader, Ce
 			Exceptions.ignore(e);
 		}
 		return (crlObject != null ? convertCRL(crlObject) : null);
+	}
+
+	private static byte[] encryptKey(KeyPair key, String resource, PasswordCallback newPassword) throws IOException {
+		char[] passwordChars = newPassword.queryPassword(resource);
+
+		if (passwordChars == null) {
+			throw new PasswordRequiredException(resource);
+		}
+
+		byte[] encoded;
+
+		try {
+			PKCS8EncryptedPrivateKeyInfoBuilder encryptedPrivateKeyInfoBuilder = new PKCS8EncryptedPrivateKeyInfoBuilder(
+					KeyHelper.encodePrivateKey(key.getPrivate()));
+			OutputEncryptor encryptor = OUTPUT_ENCRYPTOR_BUILDER.build(passwordChars);
+
+			encoded = encryptedPrivateKeyInfoBuilder.build(encryptor).getEncoded();
+		} catch (OperatorCreationException e) {
+			throw new CertProviderException(e);
+		}
+		return encoded;
 	}
 
 	@Override
