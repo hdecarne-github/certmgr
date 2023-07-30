61909e576c233e989711173ae8a7681f0cc818bf
Holger <hdecarne@gmail.com>
2023-07-30 17:15:53 +0200

    Update copyright notice

--- internal/certmgr/certmgr.go
+++ internal/certmgr/certmgr.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2015-2023 Holger de Carne
+// Copyright (C) 2015-2023 Holger de Carne and contributors
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
--- pkg/keys/keys.go
+++ pkg/keys/keys.go
@@ -1,8 +1,9 @@
-// Copyright (C) 2015-2023 Holger de Carne
+// Copyright (C) 2015-2023 Holger de Carne and contributors
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
 
+// Package keys implements a unified interface for key pair handling.
 package keys
 
 import (
@@ -12,20 +13,14 @@ import (
 	"crypto/rsa"
 )
 
-func PrivatesEqual(key1 crypto.PrivateKey, key2 crypto.PrivateKey) bool {
-	ecdsaKey1, ok := key1.(*ecdsa.PrivateKey)
-	if ok {
-		return ecdsaKey1.Equal(key2)
-	}
-	ed25519Key1, ok := key1.(ed25519.PrivateKey)
-	if ok {
-		return ed25519Key1.Equal(key2)
-	}
-	rsaKey1, ok := key1.(*rsa.PrivateKey)
-	if ok {
-		return rsaKey1.Equal(key2)
-	}
-	return false
+type KeyPair interface {
+	Public() crypto.PublicKey
+	Private() crypto.PrivateKey
+}
+
+type KeyPairFactory interface {
+	Name() string
+	New() (KeyPair, error)
 }
 
 func PublicsEqual(key1 crypto.PublicKey, key2 crypto.PublicKey) bool {
@@ -38,6 +33,22 @@ func PublicsEqual(key1 crypto.PublicKey, key2 crypto.PublicKey) bool {
 		return ed25519Key1.Equal(key2)
 	}
 	rsaKey1, ok := key1.(*rsa.PublicKey)
+	if ok {
+		return rsaKey1.Equal(key2)
+	}
+	return false
+}
+
+func PrivatesEqual(key1 crypto.PrivateKey, key2 crypto.PrivateKey) bool {
+	ecdsaKey1, ok := key1.(*ecdsa.PrivateKey)
+	if ok {
+		return ecdsaKey1.Equal(key2)
+	}
+	ed25519Key1, ok := key1.(ed25519.PrivateKey)
+	if ok {
+		return ed25519Key1.Equal(key2)
+	}
+	rsaKey1, ok := key1.(*rsa.PrivateKey)
 	if ok {
 		return rsaKey1.Equal(key2)
 	}
